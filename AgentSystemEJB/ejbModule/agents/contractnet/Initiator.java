8857f0785a9d2ca016065a0e93bc1a1cd1ba3829
nenad <nesa993@gmail.com>
2016-06-01 15:28:42 +0200

    Contranct net protocol implemented

--- AgentSystemEJB/ejbModule/agents/contractnet/Initiator.java
+++ AgentSystemEJB/ejbModule/agents/contractnet/Initiator.java
@@ -1,5 +1,6 @@
 package agents.contractnet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.ejb.EJB;
@@ -29,19 +30,69 @@ public class Initiator extends SirAgent
 	@EJB
 	MessageLoggerRemote logger;
 	
+	private ArrayList<Proposal> pendingProposals;
+	
+	private int numOfParticipants;
+	
+	private int numOfProposals;
+	
+	public Initiator()
+	{
+		pendingProposals = new ArrayList<Proposal>();
+		numOfParticipants = 0;
+		numOfProposals = 0;
+	}
+			
 	public void callForProposal()
 	{
-		//why the fuck doesn't this work?
-		//	AIDS[] runningAgents = (AIDS[]) agentManager.allAgents().values().toArray();
-		
-		
 		AIDS[] runningAgents = arrayOfAgents();
+		
+		Proposal proposal = new Proposal();
+		proposal.setInitiator(aids);
 		
 		ACLMessage msg = new ACLMessage();
 		msg.setPerformative(Performative.CALL_FOR_PROPOSAL);
+		msg.setContentObject(proposal);
 		
 		sendToAllReceivers(runningAgents, msg);
 		
+	}
+	
+	public void addProposal(ACLMessage msg)
+	{
+		logger.logMessage("Got a proposal back from agent " + msg.getReceivers()[0].getName());
+		
+		Proposal participantProposal = (Proposal) msg.getContentObject();
+		
+		pendingProposals.add(participantProposal);
+		
+		numOfProposals++;
+		
+		//ako je broj vracenih proposala jednak ukupnom broju poslatih znaci da su se svi javili
+		if(numOfProposals == numOfParticipants)
+		{
+			Proposal bestProposal = getBestProposal();
+			
+			logger.logMessage("Agent " + bestProposal.getParticipant().getName() + " had the best proposal  "
+							+ bestProposal.getTimeEstimate());
+			
+			//posaljemo poruku nazad Participant sa najboljom ponudom
+			ACLMessage msgToParticipant = new ACLMessage();
+			msgToParticipant.setPerformative(Performative.ACCEPT_PROPOSAL);
+			msgToParticipant.setReceivers(new AIDS[] { aids } );
+			msgToParticipant.setSender(bestProposal.getParticipant());
+			
+			
+			
+			messageManager.post(msgToParticipant);
+			
+		}
+	}
+	
+	private void taskDone(ACLMessage msg)
+	{
+		logger.logMessage("Agent " + msg.getReceivers()[0].getName() + "has done the task he was assinged");
+		resetValues();
 	}
 	
 	@Override
@@ -53,13 +104,13 @@ public class Initiator extends SirAgent
 			callForProposal();
 			break;
 		case PROPOSE:
-			// proposal
+			addProposal(msg);
 			break;
 		case REFUSE:
 			//refuse propsal
 			break;
 		case INFORM:
-			//we are done
+			taskDone(msg);
 			break;
 		case FAILURE:
 			//fail
@@ -71,23 +122,29 @@ public class Initiator extends SirAgent
 		}
 	}
 	
-	public AIDS[] arrayOfAgents()
+	//Returns a array of all running participants agents
+	private AIDS[] arrayOfAgents()
 	{
 		HashMap<String, AIDS> agentsAids = (HashMap<String, AIDS>) agentManager.allAgents();
 		
-		AIDS[] aidsArray =  new AIDS[agentsAids.values().size()];
-		int i = 0;
+		ArrayList<AIDS> participants =  new ArrayList<AIDS>();
 		
+		//get just the participant agents
 		for(AIDS a : agentsAids.values())
 		{
-			aidsArray[i] = a;
-			i++;
+			if(a.getType().getName().equals("Participant"))
+			{
+				participants.add(a);
+			}
+			
 		}
 		
-		return aidsArray;
+		AIDS[] aidsArray =  new AIDS[participants.size()];
+		
+		return participants.toArray(aidsArray);
 	}
 	
-	public void sendToAllReceivers(AIDS[] receivers, ACLMessage msg)
+	private void sendToAllReceivers(AIDS[] receivers, ACLMessage msg)
 	{
 		for(AIDS recvAids : receivers)
 		{
@@ -99,6 +156,36 @@ public class Initiator extends SirAgent
 			messageManager.post(msg);
 		}
 		
-		logger.logMessage("Send a request for proposal to all agents");
+		numOfParticipants = receivers.length;
+		
+		logger.logMessage("Agent " + aids.getName() + " sent a proposal to " 
+				+ String.valueOf(numOfParticipants) + " Participant agents");
+	}
+	
+	/**
+	 *  Retturn a proposal with minimun timeEstimate
+	 */
+	private Proposal getBestProposal()
+	{
+		Proposal min = new Proposal();
+		min.setTimeEstimate(100);
+		
+		for(Proposal p : pendingProposals)
+		{
+			if(p.getTimeEstimate() < min.getTimeEstimate())
+			{
+				min = p;
+				min.setParticipant(p.getParticipant());
+			}
+		}
+		
+		return min;
+	}
+	
+	private void resetValues()
+	{
+		pendingProposals.clear();;
+		numOfParticipants = 0;
+		numOfProposals = 0;
 	}
 }
--- AgentSystemEJB/ejbModule/agents/contractnet/Participant.java
+++ AgentSystemEJB/ejbModule/agents/contractnet/Participant.java
@@ -1,12 +1,17 @@
 package agents.contractnet;
 
+import java.util.Random;
+
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateful;
 
 import interfaces.Agent;
 import interfaces.MessageLoggerRemote;
+import interfaces.MessageManagerRemote;
 import model.ACLMessage;
+import model.AIDS;
+import model.Performative;
 import model.SirAgent;
 
 @Stateful
@@ -17,9 +22,38 @@ public class Participant extends SirAgent
 	@EJB
 	MessageLoggerRemote logger;
 	
-	public void respondToProposal()
+	@EJB
+	MessageManagerRemote messageManager;
+	
+	public void respondToProposal(ACLMessage msg)
 	{
-		logger.logMessage("Got a request for a proposal from the initiator");
+		logger.logMessage("Agent " + aids.getName() + " got a request for a proposal from " 
+						+ msg.getReceivers()[0].getName());
+		
+		long timeEstimate = getEstimate();
+		
+		Proposal proposalResponse = new Proposal();
+		proposalResponse.setTimeEstimate(timeEstimate);
+		proposalResponse.setParticipant(aids);
+		
+		ACLMessage proposalMsg = new ACLMessage();
+		proposalMsg.setPerformative(Performative.PROPOSE);
+		proposalMsg.setContentObject(proposalResponse);
+		proposalMsg.setSender(msg.getReceivers()[0]);
+		proposalMsg.setReceivers(new AIDS[] { aids } );
+		
+		messageManager.post(proposalMsg);
+	}
+	
+	private void acceptProposal(ACLMessage msg)
+	{
+		ACLMessage informMsg =  new ACLMessage();
+		informMsg.setPerformative(Performative.INFORM);
+		informMsg.setSender(msg.getReceivers()[0]);
+		informMsg.setReceivers(new AIDS[] { aids } );
+		
+		messageManager.post(informMsg);
+	
 	}
 	
 	@Override
@@ -28,18 +62,25 @@ public class Participant extends SirAgent
 		switch(msg.getPerformative())
 		{
 			case CALL_FOR_PROPOSAL:
-				respondToProposal();
+				respondToProposal(msg);
 				break;
 			case REJECT_PROPOSAL:
 				// handleRejectProposal();
 				break;
 			case ACCEPT_PROPOSAL:
-				//acceptProposal
+				acceptProposal(msg);
 				break;
 			default:
 				break;
 		}
 		
-			
+	}
+	
+	//returns a number between 1 and 100
+	public long getEstimate()
+	{
+		Random r = new Random();
+		
+		return (long) (r.nextInt(100) + 1);
 	}
 }
